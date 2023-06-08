ecc31bbfd49f270ffd18bb921e7cb4c536588b88
zaqihdyt13 <135988286+zaqihdyt13@users.noreply.github.com>
2023-06-08 22:07:50 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,31 @@
+var nama = prompt("Masukkan nama anda!");
+document.getElementById("nama").innerText = nama;
+
+window.onscroll = () => {
+  let header = document.querySelector(".navbar");
+
+  header.classList.toggle("sticky", window.scrollY > 100);
+};
+
+function validateForm() {
+  const name = document.forms["message-form"]["full-name"].value;
+  const birthDate = document.forms["message-form"]["birth-date"].value;
+  const gender = document.forms["message-form"]["gender"].value;
+  const messages = document.forms["message-form"]["messages"].value;
+
+  if (name == "" || birthDate == "" || gender == "" || messages == "") {
+    alert("Tidak boleh ada yang kosong");
+    return false;
+  }
+
+  setSenderUI(name, birthDate, gender, messages);
+
+  return false;
+}
+
+function setSenderUI(name, birthDate, gender, messages) {
+  document.getElementById("sender-full-name").innerHTML = name;
+  document.getElementById("sender-birth-date").innerHTML = birthDate;
+  document.getElementById("sender-gender").innerHTML = gender;
+  document.getElementById("sender-messages").innerHTML = messages;
+}
